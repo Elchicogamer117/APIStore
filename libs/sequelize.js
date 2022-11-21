09434c5551ba06ae61cc7bbaa5ac5b2778880f5c
Cristian <cris18002011@hotmail.com>
2022-11-21 12:53:09 -0600

    Prueba ORM: Cambio a MySQL

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -4,10 +4,12 @@ const configuracionModelos = require('../db/models');
 
 const USUARIO = encodeURIComponent(config.dbUser);
 const CONTRASEÑA = encodeURIComponent(config.dbPassword);
-const URL = `postgres://${USUARIO}:${CONTRASEÑA}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const URL = `postgres://${USUARIO}:${CONTRASEÑA}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URL = `mysql://${USUARIO}:${CONTRASEÑA}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URL, {
-  dialect: 'postgres',
+  // dialect: 'postgres',
+  dialect: 'mysql',
   logging: true
 });
 
